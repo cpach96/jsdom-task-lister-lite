6c6ae0580df46146e9e5c9a14ebf15c6f1972b84
cpach96 <cpach96@gmail.com>
2021-03-04 10:28:22 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const input = document.querySelector("input")
+  const log = document.getElementById("new-task-description")
+  input.addEventListener("input", updateValue);
+  function updateValue(e){
+    log.textContent = e.target.value;
+  }
 });
+
+
+document.getElementById("button").addEventListener("click", function(event){
+  event.preventDefault()
+  const log = document.getElementById("new-task-description")
+  const ul = document.getElementById("tasks")
+  const li = document.createTextNode(log.textContent)
+  ul.appendChild(li)
+});
+
